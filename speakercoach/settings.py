9461d99ec1139cb540756f5bd423d9b7a953a052
kub1c3k <jakubkubala729@gmail.com>
2026-03-13 19:14:40 +0100

    security fix: Remove hardcoded email credentials

--- speakercoach/settings.py
+++ speakercoach/settings.py
@@ -96,9 +96,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'jakubkubala729@gmail.com'
-EMAIL_HOST_PASSWORD = 'wfmq yyto uzgt myac'
-
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
 LOGIN_REDIRECT_URL = 'landingpage'
